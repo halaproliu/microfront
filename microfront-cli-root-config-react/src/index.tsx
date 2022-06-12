b4fd502b5266984468fe396ed69dab085b96ae4e
liuwenjian <liuwenjian@bilibili.com>
2022-06-12 21:09:49 +0800

    添加模块热更新

--- microfront-cli-root-config-react/src/index.tsx
+++ microfront-cli-root-config-react/src/index.tsx
@@ -4,11 +4,21 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './single-spa-config'
 
-const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-)
+const runApp = (): void => {
+  const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  )
+}
+
+runApp()
+
+if ((module as any).hot) {
+  (module as any).hot.accept('./App', () => {
+    runApp()
+  })
+}
