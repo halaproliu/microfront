a8dcc05715401a5d91124373816427d717a74cd8
liuwenjian <liuwenjian@bilibili.com>
2022-06-12 12:30:55 +0800

    添加esbuild

--- microfront-cli-root-config-react/config/webpack.config.js
+++ microfront-cli-root-config-react/config/webpack.config.js
@@ -2,52 +2,42 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const WebpackBar = require('webpackbar')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const webpack = require('webpack')
+const esbuild = require('esbuild')
+const { ESBuildMinifyPlugin } = require('esbuild-loader')
 
 const resolve = (dir) => {
   return path.resolve(process.cwd(), dir)
 }
 
 module.exports = {
-  entry: path.join(__dirname, 'src/index'),
+  entry: path.join(process.cwd(), 'src/index'),
   cache: false,
 
   mode: 'development',
   devtool: 'source-map',
-
-  optimization: {
-    runtimeChunk: true,
-    removeAvailableModules: false,
-    removeEmptyChunks: false,
-    splitChunks: false,
-    usedExports: true,
-    // splitChunks: {
-    //   cacheGroups: {//缓存组，这里是我们表演的舞台，抽取公共模块什么的，都在这个地方
-    //     defaultVendors: {
-    //       test: /[\\/]node_modules[\\/]/,
-    //       priority: -10,//优先级
-    //       reuseExistingChunk: true,
-    //     },
-    //     default: {
-    //       minChunks: 2,
-    //       priority: -20,
-    //       reuseExistingChunk: true,
-    //     },
-    //   }
-    // }
-  },
   output: {
     publicPath: 'http://localhost:9000/',
     filename: '[name].js',
+    path: resolve('dist')
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new ESBuildMinifyPlugin({
+        target: 'es2015',
+        legalComments: 'none', // 去除注释
+        css: true, // 压缩 css
+        implementation: esbuild // 自定义 esbuild instance 实现
+      })
+    ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
     alias: {
-      '@': resolve('src'),
-    },
+      '@': resolve('src')
+    }
   },
   devServer: {
     port: 9000,
@@ -65,22 +55,20 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       },
       {
         test: /\.styl$/,
-        use: ['style-loader', 'css-loader', 'stylus-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'stylus-loader']
       }
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({ template: './public/index.html' }),
     new ESLintPlugin({
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
+      extensions: ['js', 'jsx', 'ts', 'tsx']
     }),
     new WebpackBar(),
-    new FriendlyErrorsPlugin(),
     new MiniCssExtractPlugin(),
     new CleanWebpackPlugin()
   ]
---
+++ microfront-cli-root-config-react/config/webpack.dev.config.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge')
+const baseConfig = require('./webpack.config.js')
+const webpack = require('webpack')
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+})
