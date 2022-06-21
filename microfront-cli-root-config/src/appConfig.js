c5ee4de8dae73dc67f15b9deb41906ec7ae8e8e7
liuwenjian <liuwenjian@bilibili.com>
2022-06-22 00:51:42 +0800

    修改部署域名

--- microfront-cli-root-config/src/appConfig.js
+++ microfront-cli-root-config/src/appConfig.js
@@ -3,6 +3,7 @@ import EventBusService from './libs/rxjs'
 export const EventBus = new EventBusService()
 
 const type = 'history'
+const idPrd = process.env.NODE_ENV === 'production'
 
 const activeWhen = (location, key) => {
   if (type === 'history') {
@@ -11,23 +12,26 @@ const activeWhen = (location, key) => {
     return location.hash.startsWith(`#${key}`)
   }
 }
+const getHost = (port) => {
+  return isPrd ? `http://124.223.2.144:${port}` : `http://localhost:${port}`
+}
 const apps = [
   {
-    host: 'http://localhost:9001',
+    host: getHost(9001),
     projectName: 'singleVue',
     activeWhen: (location) => activeWhen(location, '/vue'),
     customProps: { EventBus },
     bundle: 'app'
   },
   {
-    host: 'http://localhost:9002',
+    host: getHost(9002),
     projectName: 'singleVue2',
     activeWhen: (location) => activeWhen(location, '/vue2'),
     customProps: { EventBus },
     bundle: 'app'
   },
   {
-    host: 'http://localhost:9003',
+    host: getHost(9003),
     projectName: 'singleReact',
     activeWhen: (location) => activeWhen(location, '/react'),
     customProps: { EventBus },
