326c882d27ad0921024e4d0ccadc2c2ca7af5cc7
liuwenjian <liuwenjian@bilibili.com>
2022-06-23 13:13:31 +0800

    upate

--- microfront-cli-react-utils/del.sh
+++ microfront-cli-react-utils/del.sh
@@ -16,7 +16,7 @@ else
   # 删除容器
   docker rm $containerid
   # 删除镜像
-  docker images | grep reactutils | awk '${print $3}' | xargs docker rmi
+  docker images | grep reactutils | awk '{print $3}' | xargs docker rmi
   # 编译镜像
   npm run image
   # 发布容器
