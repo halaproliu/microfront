2364d6ecc389780cbca307795068efe66dcc34f2
liuwenjian <liuwenjian@bilibili.com>
2022-06-23 13:01:42 +0800

    修改脚本

--- microfront-cli-react-utils/del.sh
+++ microfront-cli-react-utils/del.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-containerid=$(docker ps -a | grep reactutils | awk '{print $1}')
-imageid=$(docker images | grep reactutils | awk '${print $3}')
+# 获取containerId
+containerid=docker ps -a | grep reactutils | awk '{print $1}'
+# 获取镜像ID
+# imageid=docker images | grep reactutils | awk '${print $3}'
+# 停止容器
 docker stop $containerid
+# 删除容器
 docker rm $containerid
-docker rmi $imageid
+# 删除镜像
+docker images | grep reactutils | awk '${print $3}' | xargs docker rmi
+# 编译镜像
 npm run image
+# 发布容器
 npm run deploy
