c3018b61d8c76a48f9ec7a6b0761c4ed152418f9
liuwenjian <liuwenjian@bilibili.com>
2022-06-23 13:12:57 +0800

    修改shell条件判断

--- microfront-cli-react-utils/del.sh
+++ microfront-cli-react-utils/del.sh
@@ -8,16 +8,17 @@ then
   npm run image
   # 发布容器
   npm run deploy
+else
+  # 获取镜像ID
+  # imageid=docker images | grep reactutils | awk '${print $3}'
+  # 停止容器
+  docker stop $containerid
+  # 删除容器
+  docker rm $containerid
+  # 删除镜像
+  docker images | grep reactutils | awk '${print $3}' | xargs docker rmi
+  # 编译镜像
+  npm run image
+  # 发布容器
+  npm run deploy
 fi
-# 获取镜像ID
-# imageid=docker images | grep reactutils | awk '${print $3}'
-# 停止容器
-docker stop $containerid
-# 删除容器
-docker rm $containerid
-# 删除镜像
-docker images | grep reactutils | awk '${print $3}' | xargs docker rmi
-# 编译镜像
-npm run image
-# 发布容器
-npm run deploy
